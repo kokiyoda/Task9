34518b17b06d9951e389fb6b958321f4b51cf1c6
koki yoda <134734832+kokiyoda@users.noreply.github.com>
2023-07-10 05:51:00 +0900

    first commit

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS pokemons;
+
+CREATE TABLE pokemons (
+  id int unsigned AUTO_INCREMENT,
+  pokedexNo int unsigned NOT NULL,
+  name VARCHAR(6) NOT NULL,
+  nickname VARCHAR(6),
+  PRIMARY KEY(id)
+);
+
+INSERT INTO pokemons (pokedexNo, name, nickname) VALUES (1, "フシギダネ", "ふしぎ");
+INSERT INTO pokemons (pokedexNo, name) VALUES (4, "ヒトカゲ", "ドン");
+INSERT INTO pokemons (pokedexNo, name, nickname) VALUES (7, "ゼニガメ", "かめ");
+INSERT INTO pokemons (pokedexNo,name) VALUES (16, "ポッポ");
