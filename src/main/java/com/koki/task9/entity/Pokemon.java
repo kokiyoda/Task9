9ee3821ab06a2b6edf05e7f34280db482aae51d3
koki yoda <134734832+kokiyoda@users.noreply.github.com>
2023-07-10 23:55:20 +0900

    @Getterアノテーションをクラスにつける

--- src/main/java/com/koki/task9/entity/Pokemon.java
+++ src/main/java/com/koki/task9/entity/Pokemon.java
@@ -2,14 +2,15 @@ package com.koki.task9.entity;
 
 import lombok.Getter;
 
+@Getter
 public class Pokemon {
-    @Getter
+
     private int id;
-    @Getter
+
     private int pokedexNo;
-    @Getter
+
     private String name;
-    @Getter
+
     private String nickname;
 
     public Pokemon(int id, int pokedexNo, String name, String nickname) {
