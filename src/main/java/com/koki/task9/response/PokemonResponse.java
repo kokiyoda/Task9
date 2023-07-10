06d60732e0efcb9203a47e7d5a72103a7b11a591
koki yoda <134734832+kokiyoda@users.noreply.github.com>
2023-07-11 00:03:05 +0900

    レスポンスでidも返すように変更

--- src/main/java/com/koki/task9/response/PokemonResponse.java
+++ src/main/java/com/koki/task9/response/PokemonResponse.java
@@ -3,15 +3,19 @@ package com.koki.task9.response;
 import com.koki.task9.entity.Pokemon;
 import lombok.Getter;
 
+@Getter
 public class PokemonResponse {
-    @Getter
+
+    private int id;
+
     private int pokedexNo;
-    @Getter
+
     private String name;
-    @Getter
+
     private String nickname;
 
     public PokemonResponse(Pokemon pokemon) {
+        this.id = pokemon.getId();
         this.pokedexNo = pokemon.getPokedexNo();
         this.name = pokemon.getName();
         this.nickname = pokemon.getNickname();
